9b0f0eb0853b273c9195efc26306c693a516567c
MurtoHilali <46253642+MurtoHilali@users.noreply.github.com>
2021-05-12 21:45:26 -0400

    Update genome_vis.py
    Modified dimensions function so that the array can at most be 5x wider than it is tall; this prevent generating an image that is too wide and short to actually see.

--- genome_vis.py
+++ genome_vis.py
@@ -3,36 +3,37 @@ from PIL import Image
 import math
 
 ## open genome txt file as string 'seq'
-with open(r"h_cinaedi.txt", 'r') as file:
+with open(r"c_l_familiaris_c38.txt", 'r') as file:
     seq = file.readlines()[1:]
     seq = ''.join(seq).replace('\n','')
 
 ##RGB colours for each base
 colours = {
-    "A":[98, 60, 234],
-    "T":[136, 90, 90],
-    "C":[244, 185, 66],
-    "G":[75, 192, 217]
+    "A":[85, 61, 54],
+    "T":[255, 200, 87],
+    "C":[216, 71, 151],
+    "G":[135, 160, 178]
 }
 
-## function to return the two closest integer factors of seq_length
+## return the two closest integer factors of seq_length
+## such that one is never more than 5x larger than the other
 def dimensions(seq):
     seq_length = len(seq)
     a = int(math.sqrt(seq_length))
     while seq_length % a != 0:
         a += 1
-    if a == seq_length:
+    if a == seq_length or a >= (seq_length // a) * 5:
         vals = [dimensions(seq[:-1])[0], dimensions(seq[:-1])[1], seq[:-1]]
         return vals
     else:
-        vals = [a, int(seq_length / a)]
+        vals = [a, seq_length // a]
         return vals
 
-## generates array with dimensions from dimensions() containing RGB values from colour {}
+## generates array with dimensions from dimensions() containing RGB values from colours {}
 def generate(h, w, data):
     for i in range(h):
         for j in range(w):
-            data[i,j] = colours[seq[j+i*w]]
+            data[i,j] = colours3[seq[j+i*w]]
     return data
 
 ## assign height and width of image
@@ -50,5 +51,5 @@ data = generate(h, w, data)
 
 ## convert array into png image
 img = Image.fromarray(data, 'RGB')
-img.save('e_coli_new.png')
+img.save('c_l_familiaris_c38.png')
 img.show()
