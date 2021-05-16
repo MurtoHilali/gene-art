2c4f6888702dcff7156611c952b43fdee37177f3
MurtoHilali <46253642+MurtoHilali@users.noreply.github.com>
2021-05-16 14:51:17 -0400

    Update genome_vis.py
    Simplified the dimensions function and removed unnecessary lines of code.

--- genome_vis.py
+++ genome_vis.py
@@ -9,39 +9,37 @@ with open(r"c_l_familiaris_c38.txt", 'r') as file:
 
 ##RGB colours for each base
 colours = {
-    "A":[85, 61, 54],
-    "T":[255, 200, 87],
-    "C":[216, 71, 151],
-    "G":[135, 160, 178]
+    "A":[170, 172, 176],
+    "T":[182, 201, 187],
+    "C":[191, 237, 193],
+    "G":[27, 47, 51],
+    "N":[252, 163, 17]
 }
 
-## return the two closest integer factors of seq_length
-## such that one is never more than 5x larger than the other
+## function to return the two closest integer factors of seq_length
+## with a minimum ration of 1:5
 def dimensions(seq):
     seq_length = len(seq)
     a = int(math.sqrt(seq_length))
     while seq_length % a != 0:
         a += 1
-    if a == seq_length or a >= (seq_length // a) * 5:
-        vals = [dimensions(seq[:-1])[0], dimensions(seq[:-1])[1], seq[:-1]]
-        return vals
+    if a >= (seq_length // a) * 5:
+        vals = [dimensions(seq[:-1])[0], dimensions(seq[:-1])[1]]
     else:
         vals = [a, seq_length // a]
-        return vals
+    return vals
 
-## generates array with dimensions from dimensions() containing RGB values from colours {}
+## generates array with dimensions from dimensions() containing RGB values from colour {}
 def generate(h, w, data):
     for i in range(h):
         for j in range(w):
-            data[i,j] = colours3[seq[j+i*w]]
+            data[i,j] = colours[seq[j+i*w]]
     return data
 
 ## assign height and width of image
 vals = dimensions(seq)
 h = min(vals[:2])
 w = max(vals[:2])
-if len(vals) == 3:
-    seq = vals[2]
 
 ## create empty array using h and w as dimensions
 data = np.zeros((h,w,3), dtype=np.uint8)
@@ -51,5 +49,5 @@ data = generate(h, w, data)
 
 ## convert array into png image
 img = Image.fromarray(data, 'RGB')
-img.save('c_l_familiaris_c38.png')
+img.save('c_l_familiaris_c38_test_3.png')
 img.show()
